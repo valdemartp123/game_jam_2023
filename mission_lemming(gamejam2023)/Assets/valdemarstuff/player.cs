1e739230ac7d5a3e99e8d0dc04689e2b901439a1
valdemartp123 <118601275+valdemartp123@users.noreply.github.com>
2023-11-11 16:59:40 +0100

    update

--- mission_lemming(gamejam2023)/Assets/valdemarstuff/player.cs
+++ mission_lemming(gamejam2023)/Assets/valdemarstuff/player.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     Rigidbody playerrb;
-    
+    public GameObject pivot;
     float vinput;
     public GameObject cameraobject;
+    public float jumpheight;
+    bool touchground;
 
     //coolstuff
     
@@ -21,9 +23,30 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        gameObject.transform.rotation = pivot.transform.rotation;
         vinput = Input.GetAxis("Vertical");
         
         playerrb.AddForce(cameraobject.transform.forward * speed * vinput * Time.deltaTime);
 
+        if (  touchground==true&& Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Debug.Log("if");
+            playerrb.AddForce(Vector3.up * jumpheight * Time.deltaTime, ForceMode.Impulse);
+            touchground = false; 
+        }
+
+
     }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            Debug.Log("collision");
+            touchground = true;
+        }
+    }
+
+
 }

a74060ef21316d7d081a655892e7e01870b13797
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2023-11-12 08:52:49 +0100

    death script

--- mission_lemming(gamejam2023)/Assets/Euans Scripts/DestroyOnCollision.cs
+++ mission_lemming(gamejam2023)/Assets/Euans Scripts/DestroyOnCollision.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DestroyOnCollision : MonoBehaviour
-{
+public class DestroyOnCollision : MonoBehaviour{
+
+bool isplayer;
+
 void start(){
-    bool isplayer = false;
-}
-   private void OnCollisionEnter(Collision collision)
-   {
-    if(gameObject==GameObject.Find("player")){
+if(gameObject==GameObject.Find("player")){
         isplayer=true;
     }else{
         isplayer=false;
     }
+}
+
+   private void OnCollisionEnter(Collision collision)
+   {
+   
     if (collision.collider.CompareTag("deadly"))
     {
         if (isplayer = true){
-            SceneManager.LoadScene (gameover);
+            SceneManager.LoadScene ("gameover");
         }else{
         Destroy(collision.gameObject);
         }

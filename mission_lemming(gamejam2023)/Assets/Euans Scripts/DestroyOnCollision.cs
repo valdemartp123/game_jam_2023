db9eff029e35f5876ad57f3e9a314a4812342896
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2023-11-12 10:41:03 +0100

    Death Audio

--- mission_lemming(gamejam2023)/Assets/Euans Scripts/DestroyOnCollision.cs
+++ mission_lemming(gamejam2023)/Assets/Euans Scripts/DestroyOnCollision.cs
@@ -6,24 +6,29 @@ using UnityEngine.SceneManagement;
 public class DestroyOnCollision : MonoBehaviour{
 
 bool isplayer;
-
+public AudioSource source;
+public AudioClip clip;
 void Start(){
 if(gameObject==GameObject.Find("player")){
         isplayer=true;
     }else{
         isplayer=false;
     }
+    source=GetComponent<AudioSource>();
 }
 
    private void OnCollisionEnter(Collision collision)
    {
    
-    if (collision.gameObject.CompareTag("deadly"))
+    if (collision.gameObject.tag=="deadly")
     {
+        Debug.Log("spil lyd");
         if (isplayer == true){
         SceneManager.LoadScene ("gameover");
         }else{
-        Destroy(gameObject);
+        source.Play();
+        //source.PlayOneShot(clip);
+        Destroy(gameObject,1.5f);
         ScoreManager.instance.RemovePoint();
         }
     }
